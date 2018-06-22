d35d8c29c8800e3cc6b9c715d50592c4b4a55d06
Patrick Boettcher <patrick.boettcher@posteo.de>
2018-06-22 17:12:37 +0200

    improve column-script

--- columnize.sh
+++ columnize.sh
@@ -7,30 +7,64 @@ DESKTOP=$($X get_desktop)
 WIDTH=$($X getdisplaygeometry | awk '{ print $1; }')
 HEIGHT=$($X getdisplaygeometry | awk '{ print $2; }')
 
-WINDOWS=($($X search --desktop $DESKTOP 'term'))
-WINDOWS=($WINDOWS $($X search --desktop $DESKTOP 'vim'))
-COUNT=${#WINDOWS[@]}
+# get terminals
+TERMS=($($X search --desktop $DESKTOP 'term'))
+VIMS=($($X search --desktop $DESKTOP 'vim'))
+GITS=($($X search --desktop $DESKTOP 'git'))
 
-if [ $COUNT -gt 4 ]
-then
-	echo too many windows to create columns
-	exit 1
-fi
-
-if [ $COUNT -lt 3 ]
-then
-	COUNT=3
-fi
-
-echo $WIDTH
-
-WIDTH=$(($WIDTH/$COUNT))
 POS=0
 
-for W in ${WINDOWS[@]}
+# 1/4 for the terminal
+TERMWIDTH=$(($WIDTH/4))
+GITWIDTH=$(($WIDTH/4))
+
+VIMCOLSMAX=3
+VIMWIDTH=$(($WIDTH - $TERMWIDTH))
+
+# if git, reserve one column for it
+if [ ${#GITS[@]} -gt 0 ]
+then
+	VIMCOLSMAX=$((VIMCOLSMAX - 1))
+	VIMWIDTH=$((VIMWIDTH - $GITWIDTH))
+fi
+
+# effective vim columns
+VIMCOLS=${#VIMS[@]}
+if [ $VIMCOLS -lt 2 ]
+then
+	VIMCOLS=$VIMCOLSMAX
+fi
+if [ $VIMCOLS -gt $VIMCOLSMAX ]
+then
+	VIMCOLS=$VIMCOLSMAX
+fi
+VIMWIDTH=$((VIMWIDTH / $VIMCOLS))
+
+# align terminals
+for W in ${TERMS[@]}
 do
-	EFF=$((WIDTH - 8))
-	$X windowsize $W $EFF $HEIGHT
-	$X windowmove $W $POS 0
-	POS=$(($POS + $WIDTH + 8))
+	$X windowsize $W $TERMWIDTH $HEIGHT
+	$X windowmove $W $POS 55
+	POS=$((POS + $TERMWIDTH))
+done
+
+# align vims
+VIMCOUNT=0
+for W in ${VIMS[@]}
+do
+	$X windowsize $W $VIMWIDTH $HEIGHT
+	$X windowmove $W $POS 55
+	VIMCOUNT=$(($VIMCOUNT + 1))
+	if [ $VIMCOUNT -lt $VIMCOLSMAX ]
+	then
+		POS=$((POS + $VIMWIDTH))
+	fi
+done
+POS=$(($POS + $VIMWIDTH))
+
+# align gits
+for W in ${GITS[@]}
+do
+	$X windowsize $W $GITWIDTH $HEIGHT
+	$X windowmove $W $POS 55
 done
